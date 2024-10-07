8b5f79ff16b200785ba03a81203937c6807977ec
collins Kimani <mwas9446.ck@gmail.com>
2024-10-07 09:33:33 +0300

    add wrapper function to secure api routes
    The wrapper will be added on views where certains clients will interact with the systems so as
    to restrict access based on roles.

--- api/views/login.py
+++ api/views/login.py
@@ -6,8 +6,45 @@ import bcrypt
 import uuid
 import hashlib
 import base64
-from models.client import Client
-from run import session
+from functools import wraps
+
+def token_required(fn):
+    """wrapper fn to secure routes
+    Args:
+        fn: function being wrapped
+    Returns:
+        wrapped function with appended user details or
+        error upon token absence, invalidity or expiry
+    """
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        token = request.headers.get('access-token')
+        if not token:
+            return make_response(
+                    jsonify({'Error': 'No Token Found'}),
+                    401,
+                    {'WWW-Authenticate': 'Basic realm="Login required!"'}
+                    )
+
+        try:
+            # decode token
+            decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            # get current user in database based on the public_id
+            current_user = Client.query.filter_by(public_id=decoded_token.get('public_id')).first()
+
+            if not current_user:  # token is valid but user doesn't exist
+                return jsonify({'Error': 'User not found'}), 404
+        except jwt.ExpiredSignatureError:
+            return jsonify({'Message': 'Token Expired'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'Message': 'Invalid Token'}), 401
+        except jwt.DecodeError:
+            return jsonify({'Message': 'Error Decoding Token'}), 401
+
+        # If everything is fine, pass current_user to the wrapped function
+        return fn(current_user, *args, **kwargs)
+    return wrapper
+
 
 @app_views.route('/sign_up', methods=['POST'], strict_slashes=False)
 def sign_up() -> json:
@@ -67,8 +104,8 @@ def login() -> json:
     token = jwt.encode({
         'public_id': client.public_id,
         'exp': datetime.utcnow() + timedelta(minutes=10)
-        }, app.config['SECRET_KEY'])
+        }, app.config['SECRET_KEY'], algorithm='HS256')
     
     res = make_response(jsonify({'message': 'Login successful'}), 200)
-    res.headers['token'] = token.decode('utf-8') if isinstance(token, bytes) else token
+    res.headers['access-token'] = token.decode('utf-8') if isinstance(token, bytes) else token
     return res
